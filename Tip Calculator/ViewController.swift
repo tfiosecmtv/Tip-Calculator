76e06e942902e7866032616e502aaab7b176e3de
Aidana Iman <aidana.imangozhina@nu.edu.kz>
2020-08-11 15:26:30 +0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Aidana Imangozhina on 8/11/20.
+//  Copyright © 2020 Aidana Imangozhina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
