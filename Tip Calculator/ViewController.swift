6d0e312dac01ba9ce130c8002be93978e1e94a48
Aidana Iman <aidana.imangozhina@nu.edu.kz>
2020-08-11 16:36:14 +0500

    tip calc

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet var tipPerc: UISegmentedControl!
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var TipLabel: UILabel!
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the bill amout
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate the tip and total
+        
+        let tipPercentages = [0.1, 0.18, 0.2]
+        
+        let tip = bill*tipPercentages[tipPerc.selectedSegmentIndex]
+        let total = bill + tip
+        // Update the tip
+        
+        TipLabel.text = String(format: "$%f2", tip)
+        totalLabel.text = String(format: "$%f2", total)
+    }
+    
+    @IBAction func tapgesture(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
+    
 }
 
